d54865b833c86acaf983d58dd15c3e55fb00d772
Robin Mueller <robin.mueller.m@pm.me>
2025-08-29 17:31:05 +0200

    improvements

--- bitbybit/tests/tests.rs
+++ bitbybit/tests/tests.rs
@@ -6,6 +6,7 @@ fn all_tests() {
     t.pass("tests/basic.rs");
     t.pass("tests/with_fields.rs");
 
+    t.compile_fail("tests/no_compile/exhaustive_bitenum.rs");
     t.compile_fail("tests/no_compile/non_exhaustive_bitenum.rs");
     t.compile_fail("tests/no_compile/invalid_field_range.rs");
     t.compile_fail("tests/no_compile/invalid_field_access.rs");
